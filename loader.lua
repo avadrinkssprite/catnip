10acfd62cab1f51d5ba9f37b1b0d5b0a755cc9bd
CatThatDrinksSprite <20rcb424@gmail.com>
2026-05-30 16:34:27 -0400

    idk why it did that

--- loader.lua
+++ loader.lua
@@ -19,7 +19,7 @@ local Folders = {
 
 local Assets = {
     ["moonshine/images/cover.png"]           = "moonshine/images/embedcover.png",
-    ["moonshine/images/embedcover.png"]           = "moonshine/images/embedcover.png",
+    ["moonshine/images/embedcover.png"]      = "moonshine/images/embedcover.png",
     ["moonshine/images/moon.png"]           = "moonshine/images/moon.png",
     ["moonshine/images/paw.png"]           = "moonshine/images/paw.png",
     ["moonshine/images/W AZULA.png"]        = "moonshine/images/W%20AZULA.png",
